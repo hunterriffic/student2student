4bcef2a44cd2ebba67dc45e5a2c09c5c8b93ae6c
zachdebardlebon <zach.debardlebon@gmail.com>
2017-12-02 12:43:53 -0600

    Implemented function that returns siblings of search term.

--- Search.java
+++ Search.java
@@ -15,6 +15,7 @@ import java.util.ListIterator;
 
 public static class Search {
     protected Tag root;
+    ArrayList<User> users = new ArrayList<>();
 
     public Search(String[] tags) {
         if (terms == null)
@@ -41,54 +42,60 @@ public static class Search {
         }
     }
 
-    public Iterable<String> getMatches(String name) {
-        // TODO: Implement topKMatches
-        if (prefix == null) { throw new NullPointerException(); } //Checking if prefix is null
+    public ArrayList<User> getMatches(String name) {
+        if (name == null) {
+            throw new NullointerException();
+        } //Checking if prefix is null
         else {
-            Node cur = myRoot;
-
-            for (int i = 0; i < prefix.length(); i++) { //Iterating through the prefix
-                char c = prefix.charAt(i); //Current letter
-                if (cur.getChild(c) == null) { //Checking if cur has children
-                    return new ArrayList<String>(); //If not, returns empty list
-                }
-                cur = cur.getChild(c); //Increments cur
-            }
-            double maxWeight = cur.mySubtreeMaxWeight;
-
-            NodeComparator comp = new NodeComparator();
-            PriorityQueue<Node> q = new PriorityQueue<>(comp); //This queue will contain all nodes that will be checked if they are a word
-            PriorityQueue<Node> wordQueue = new PriorityQueue<>(comp); //Contains only words
-
-            Collection<Node> values = cur.children.values(); //Contains the children of cur
-            for (Node n : values) {
-                q.add(n); //Adding children to q
-            }
-
-            while (!q.isEmpty()) { //While q is not empty
-                if (q.peek().children.size() < 0) { continue; } //If there are no children of the specified node
-                Collection<Node> c = q.poll().children.values(); //Contains the specified node's children
-                for (Node n : c) {
-                    q.add(n); //Adding the children to q
-                    if (n.isWord) { wordQueue.add(n); } //Adding node n to wordQueue if it is a word
-                }
-            }
-
-            ArrayList<String> output = new ArrayList<String>(); //List of strings
-            ListIterator<String> listerator = output.listIterator(); //Iterator over the list
-            for (int i = 0; i < k; i++) {
-                if (!wordQueue.isEmpty()) { //While wordQueue is not empty
-                    listerator.add(wordQueue.poll().getWord()); //Add word to output list
+            ArrayList<User> output = new ArrayList<>();
+            for (User u : users) {
+                if (u.tags.contains(name)) {
+                    output.add(u);
                 }
             }
             return output;
         }
     }
 
-    class NodeComparator implements Comparator<Node> {
-        public int compare(Node a, Node b) {
-            if (a.mySubtreeMaxWeight > b.mySubtreeMaxWeight) { return -1; }
-            else if (a.mySubtreeMaxWeight < b.mySubtreeMaxWeight) { return 1; }
+    public ArrayList<Tag> getSiblings(String name) {
+        Tag cur = root;
+        ArrayList<Tag> output = ArrayList<>();
+
+        if (cur.children.contains(name)) {
+            Collection<Tag> vals = cur.children.values();
+            for (Tag t : vals) {
+                output.add(t);
+            }
+            return output;
+        }
+        else {
+            PriorityQueue<Tag> queue = new PriorityQueue<>();
+            Collection<Tag> vals = cur.children.values();
+            for (Tag t : vals) {
+                queue.add(t);
+            }
+            while (!queue.isEmpty()) {
+                Tag current = queue.poll();
+                if (current.children.contains(name)) {
+                    vals = current.children.values();
+                    for (Tag v : vals) {
+                        output.add(v);
+                    }
+                } else if (current.children.size() > 0) {
+                    vals = current.children.values();
+                    for (Tag v : vals) {
+                        queue.add(v);
+                    }
+                }
+            }
+            return output;
+        }
+    }
+
+    class EntryComparator implements Comparator<Entry> {
+        public int compare(Entry a, Entry b) {
+            if (a.date > b.date) { return -1; }
+            else if (a.date < b.date) { return 1; }
             return 0;
         }
     }
