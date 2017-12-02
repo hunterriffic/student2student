ea27f8e91f3eccc952c2e51b3c06dc41232852bc
Unknown <alyssasgable@gmail.com>
2017-12-02 16:02:13 -0600

    Database class changes for test

--- Dbconnection.java
+++ Dbconnection.java
@@ -1,28 +1,61 @@
 import java.sql.*;
 
 public class Dbconnection {
-    private static String url = "jdbc:driver://student2student.ci7oiohekotg.us-east-1.rds.amazonaws.com:3306/student2student?user=wareagle&password=rolltide";
-    private static String driverName = "com.mysql.jdbc.Driver";
-    private static String username = "wareagle";
-    private static String password = "rolltide";
-    static Connection con;
-    private static String urlstring;
+//     private static String url = "jdbc:driver://student2student.ci7oiohekotg.us-east-1.rds.amazonaws.com:3306/student2student?user=wareagle&password=rolltide";
+//     private static String driverName = "mysql-connector.jar";
+//     private static String username = "wareagle";
+//     private static String password = "rolltide";
+//     static Connection con;
+//     private static String urlstring;
+//
+//     public static Connection getConnection() {
+//         try {
+//             Class.forName(driverName);
+//             try {
+//                 con = DriverManager.getConnection(urlstring, username, password);
+//             } catch (SQLException ex) {
+//                 // log an exception. fro example:
+//                 System.out.println("Failed to create the database connection.");
+//             }
+//         } catch (ClassNotFoundException ex) {
+//             // log an exception. for example:
+//             System.out.println("Driver not found.");
+//         }
+//         return con;
+//     }
+// }
+//
 
-    public static Connection getConnection() {
-        try {
-            Class.forName(driverName);
-            try {
-                con = DriverManager.getConnection(urlstring, username, password);
-            } catch (SQLException ex) {
-                // log an exception. fro example:
-                System.out.println("Failed to create the database connection.");
-            }
-        } catch (ClassNotFoundException ex) {
-            // log an exception. for example:
-            System.out.println("Driver not found.");
-        }
-        return con;
-    }
+
+	System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		System.out.println("Where is your MySQL JDBC Driver?");
+		e.printStackTrace();
+		return;
+	}
+
+	System.out.println("MySQL JDBC Driver Registered!");
+	Connection connection = null;
+
+	try {
+		connection = DriverManager
+		.getConnection("jdbc:student2student://ci7oiohekotg.us-east-1.rds.amazonaws.com:3306/student2student","wareagle", "rolltide");
+
+	} catch (SQLException e) {
+		System.out.println("Connection Failed! Check output console");
+		e.printStackTrace();
+		return;
+	}
+
+	if (connection != null) {
+		System.out.println("You made it, take control your database now!");
+	} else {
+		System.out.println("Failed to make connection!");
+	}
+  }
 }
 
 //STEPS TO MAKE DATABASE QUERIES
